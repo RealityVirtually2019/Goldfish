4c3eaba85e24b49b01725a613742a19ce389f502
chenley390 <46662907+chenley390@users.noreply.github.com>
2019-01-19 23:40:51 -0500

    Everything

---
+++ goldfish_master/Assets/Scripts/fog.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Test()
+    {
+        //set the fog color to be blue
+        RenderSettings.fogColor = Color.blue;
+        RenderSettings.fog = true;
+    }
+
+}
